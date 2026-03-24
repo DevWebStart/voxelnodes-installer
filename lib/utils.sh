9552052643e1e3bac95e116e1f19ff5cf58bcbe6
DevWeb <zyphinweb@gmail.com>
2026-03-24 10:09:11 +0530

    Update utils.sh

--- lib/utils.sh
+++ lib/utils.sh
@@ -1,16 +1,18 @@
 #!/bin/bash
 
-check_root() {
-    if [ "$EUID" -ne 0 ]; then
-        echo "Run as root"
-        exit
-    fi
-}
+run() {
+    MSG=$1
+    CMD=$2
 
-detect_os() {
-    source /etc/os-release
-    if [[ "$ID" != "ubuntu" ]]; then
-        echo "Only Ubuntu supported"
-        exit
+    echo -ne "➤ $MSG..."
+
+    bash -c "$CMD" >> /tmp/voxel_install.log 2>&1
+
+    if [ $? -eq 0 ]; then
+        echo -e "\r✔ $MSG"
+    else
+        echo -e "\r✖ FAILED: $MSG"
+        echo "Check log: /tmp/voxel_install.log"
+        exit 1
     fi
 }
