0d3a2fd538f1a8ea71bc8c67463d00d758450dbc
DevWeb <zyphinweb@gmail.com>
2026-03-24 10:08:44 +0530

    Update installer.sh for better option handling
    Refactor installer script to improve option display and execution.

--- installer.sh
+++ installer.sh
@@ -2,30 +2,17 @@
 
 source lib/ui.sh
 source lib/utils.sh
-source modules/panel.sh
-
-check_root
-detect_os
 
 while true; do
     show_banner
-
-    echo "1) Install Panel"
-    echo "0) Exit"
+    echo -e "\e[92m1) Install Panel"
+    echo "0) Exit\e[0m"
     echo ""
-
     read -p "Select option: " opt
 
     case $opt in
-        1)
-            install_panel
-            ;;
-        0)
-            exit
-            ;;
-        *)
-            echo "Invalid"
-            sleep 1
-            ;;
+        1) bash modules/panel.sh ;;
+        0) exit ;;
+        *) echo "Invalid option"; sleep 1 ;;
     esac
 done
